f709ea90a950537e47e32b5cd5706281b9a1e8b2
KarinaOz <karina_oz@mail.ru>
2023-06-08 11:30:26 +0500

    Initial commit

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
+from dotenv.main import load_dotenv
+import os
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+load_dotenv()
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 ##CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
